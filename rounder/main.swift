62be0d392c26f070e88740ba20490e867fb2500c
Jorge Llubia <jll@latenitesoft.com>
2014-07-16 12:47:21 +0200

    Initial Commit

---
+++ rounder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  rounder
+//
+//  Created by Jorge on 16/07/14.
+//  Copyright (c) 2014 LateNiteSoft. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
